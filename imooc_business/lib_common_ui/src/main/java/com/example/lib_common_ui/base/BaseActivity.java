b9277756aae35d91aa639e526cb847d8737f5bca
waitwalker <waitwalker@163.com>
2020-08-11 16:13:40 +0800

    BaseActivity基类添加

--- imooc_business/lib_common_ui/src/main/java/com/example/lib_common_ui/base/BaseActivity.java
+++ imooc_business/lib_common_ui/src/main/java/com/example/lib_common_ui/base/BaseActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
+import com.example.lib_common_ui.utils.StatusBarUtil;
+
 public class BaseActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StatusBarUtil.statusBarLightMode(this);
     }
 }
