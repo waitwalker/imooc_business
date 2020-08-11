33763e0744531ad3bb10a9a39cf61342e9d6997e
waitwalker <waitwalker@163.com>
2020-08-11 10:48:54 +0800

    正向&反向传值处理,首页添加

---
+++ imooc_business/app/src/main/java/com/example/imooc_business/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.example.imooc_business;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HomeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        transferredParameter();
+        initData();
+        initView();
+
+    }
+
+    private void initData() {}
+
+    private void initView() {}
+
+    private void transferredParameter() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            String string = intent.getStringExtra("name");
+            Log.d("1","正向传过来的参数:" + string);
+        }
+    }
+
+    private void back() {
+        Intent intent = new Intent();
+        intent.putExtra("value", "Home");
+        setResult(1234,intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        back();
+        super.onBackPressed();
+    }
+}
--- imooc_business/app/src/main/java/com/example/imooc_business/MainActivity.java
+++ imooc_business/app/src/main/java/com/example/imooc_business/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.imooc_business;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(this, HomeActivity.class);
+                intent.putExtra("name","zhangsan");
+                startActivityForResult(intent,123);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            String string = data.getStringExtra("value");
+            Log.d("1","反向传递的参数:" + string);
+        }
     }
 }
