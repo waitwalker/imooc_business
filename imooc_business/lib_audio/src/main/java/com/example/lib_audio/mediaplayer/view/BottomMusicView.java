5266c3d82dd8ccd82ad6be40c36230181dcf68bd
waitwalker <waitwalker@163.com>
2020-08-17 14:54:05 +0800

    控件初始化&事件回调添加

--- imooc_business/lib_audio/src/main/java/com/example/lib_audio/mediaplayer/view/BottomMusicView.java
+++ imooc_business/lib_audio/src/main/java/com/example/lib_audio/mediaplayer/view/BottomMusicView.java
@@ -1,9 +1,11 @@
 package com.example.lib_audio.mediaplayer.view;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -52,6 +54,44 @@ public class BottomMusicView extends RelativeLayout {
 
     private void initView() {
         View rootView = LayoutInflater.from(mContext).inflate(R.layout.bottom_view, this);
+        rootView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 跳转到音乐播放页面
+            }
+        });
+
+        mLeftView = rootView.findViewById(R.id.album_view);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(mLeftView, View.ROTATION.getName(), 0f, 360);
+        animator.setDuration(10000);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatCount(-1);
+        animator.start();
+
+        mTitleView = rootView.findViewById(R.id.audio_name_view);
+        mAlbumView = rootView.findViewById(R.id.audio_album_view);
+        mPlayView = rootView.findViewById(R.id.play_view);
+        mPlayView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //播放暂停按钮事件
+            }
+        });
+
+        mRightView = rootView.findViewById(R.id.show_list_view);
+        mRightView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 显示音乐播放列表
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        EventBus.getDefault().unregister(this);
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN) public void onAudioLoadEvent(AudioLoadEvent event) {
